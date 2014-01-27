6994711f79667c9718687011bebc90d70d5b5bb6
Heriberto Perez <heriberto.perez@crowdint.com>
2014-01-27 13:06:37 -0600

    part 2 for the screencast

---
+++ js/collections/kindles.js
@@ -0,0 +1,3 @@
+var KindlesCollection = Backbone.Collection.extend({
+  url: 'http://localhost:3000/api/kindles'
+});
---
+++ js/models/kindle.js
@@ -0,0 +1,3 @@
+var KindleModel = Backbone.Model.extend({
+  url: 'http://localhost:3000/api/kindles.json'
+});
